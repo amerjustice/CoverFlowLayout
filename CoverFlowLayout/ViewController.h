54280940bf5c875036b6da40b3e01e820f0e0de5
amerjustice <amerjustice@outlook.com>
2016-05-19 11:15:28 -0700

    Initial Commit

---
+++ CoverFlowLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoverFlowLayout
+//
+//  Created by Susan Justice on 2016-05-19.
+//  Copyright © 2016 Amer Justice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
